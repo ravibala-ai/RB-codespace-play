3cc1242b0cc7d03f02be76dfdead12b2e23b7bdf
ravibala-ai <ravibala.speedtech.ai@gmail.com>
2025-05-07 11:54:42 +0000

    local changes from codespace

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const app = require("./app");
+const {PORT} = process.env;
+
 console.log("Hello Ravi's Codespace");
+
+app.listen(PORT, () => {
+    console.log(`SERVER is running at port: ${PORT}`);
+})
